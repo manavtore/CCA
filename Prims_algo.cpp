1353a4332826daf3f987da76463f5032c0153d3c
manavtore <113475112+manavtore@users.noreply.github.com>
2023-11-01 09:07:43 +0530

    fix: Prims_algo.cpp

--- Prims_algo.cpp
+++ Prims_algo.cpp
@@ -1,59 +1,66 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-#define V 5
+const int INF = 999999;
 
-int minKey(int key[], bool mstSet[])
-{
-    int min = INT_MAX, min_index;
+int findMinKey(vector<int>& key, vector<bool>& mstSet, int V) {
+    int minKey = INF;
+    int minIndex = -1;
 
-    for (int v = 0; v < V; v++)
-        if (mstSet[v] == false && key[v] < min)
-            min = key[v], min_index = v;
-
-    return min_index;
-}
-
-void printMST(int parent[], int graph[V][V])
-{
-    cout << "Edge \tWeight\n";
-    for (int i = 1; i < V; i++)
-        cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << " \n";
-}
-
-void primMST(int graph[V][V])
-{
-    int parent[V];
-    int key[V];
-    bool mstSet[V];
-
-    for (int i = 0; i < V; i++)
-        key[i] = INT_MAX, mstSet[i] = false;
-
-    key[0] = 0;
-    parent[0] = -1;
-
-    for (int count = 0; count < V - 1; count++) {
-        int u = minKey(key, mstSet);
-        mstSet[u] = true;
-
-        for (int v = 0; v < V; v++)
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
+    for (int v = 0; v < V; v++) {
+        if (!mstSet[v] && key[v] < minKey) {
+            minKey = key[v];
+            minIndex = v;
+        }
     }
 
-    printMST(parent, graph);
+    return minIndex;
 }
 
-int main()
-{
-    int graph[V][V] = { { 0, 2, 0, 6, 0 },
-                        { 2, 0, 3, 8, 5 },
-                        { 0, 3, 0, 0, 7 },
-                        { 6, 8, 0, 0, 9 },
-                        { 0, 5, 7, 9, 0 } };
+void primMST(vector<vector<int>& graph, int V) {
+    vector<int> parent(V);
+    vector<int> key(V, INF);
+    vector<bool> mstSet(V, false);
 
-    primMST(graph);
+    key[0] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = findMinKey(key, mstSet, V);
+
+        mstSet[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    cout << "Edge\tWeight" << endl;
+    for (int i = 1; i < V; i++) {
+        cout << parent[i] << " - " << i << "\t" << graph[i][parent[i]] << endl;
+    }
+}
+
+int main() {
+    int V;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+
+    vector<vector<int>> graph(V, vector<int>(V));
+
+    cout << "Enter the adjacency matrix: " << endl;
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    primMST(graph, V);
 
     return 0;
 }
+
